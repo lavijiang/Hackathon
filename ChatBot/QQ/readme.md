7e668abda5892ff37124f1e6e0044d3fc8983b9f
Ubuntu <lavijiang@LaviWork.yuzgulpybgdetdeaowginqhgdg.hx.internal.cloudapp.net>
2023-01-16 15:14:47 +0000

    modify info

--- ChatBot/QQ/readme.md
+++ ChatBot/QQ/readme.md
@@ -1,2 +1,20 @@
-# A Simple QQ ChatBot
+# A Simple QQ ChatBot With ChatGPT
 
+## 1 安装步骤
+### 1.1 qq bot
+https://docs.go-cqhttp.org/
+
+### 1.2 配置bot config.yml
+```
+account: # 账号相关  
+  uin: 2493233979 # QQ账号
+  password: '' # 密码为空时使用扫码登录
+```
+```
+- url: http://127.0.0.1:5701/ # 地址
+    secret: ''                # 密钥
+```
+
+## 2 启动命令
+nohup python3 main.py script.py >/dev/null 2>&1 &  
+nohup ./go-cqhttp >/dev/null 2>&1 &
--- ChatBot/QQ/script.py
+++ ChatBot/QQ/script.py
@@ -6,13 +6,45 @@ __time__ = "2022/9/9 22:04"
 
 import asyncio
 import httpx
+import openai
 from datetime import datetime
+
+
 def handle_private(uid, message):  # 处理私聊信息
+    print(message)
     if message:  # 简单的判断，只是判断其是否为空
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(send(uid, f"你好呀，请问发消息给我有什么事情?\n回复时间：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"))
-        loop.close()
-        #asyncio.run(send(uid, f"你好呀，请问发消息给我有什么事情?\n回复时间：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"))
+        asyncio.run(sendChatGPT(uid, message))
+        # asyncio.run(send(
+        #    uid, f"你好呀，请问发消息给我有什么事情?\n回复时间：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"))
+
+
+async def sendChatGPT(uid, message, gid=None):
+    """
+    用于发送消息的函数
+    :param uid: 用户id
+    :param message: 发送的消息
+    :param gid: 群id
+    :return: None
+    """
+    async with httpx.AsyncClient(base_url="http://127.0.0.1:5700") as client:
+        if gid is None:
+            # 如果发送的为私聊消息
+            openai.api_key = "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"
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=message,
+                max_tokens=2048,
+                n=1,
+                stop=None
+            )
+            print(response["choices"][0]["text"])
+            params = {
+                "user_id": uid,
+                "message": response["choices"][0]["text"],
+            }
+        await client.get("/send_private_msg", params=params)
+
+
 async def send(uid, message, gid=None):
     """
     用于发送消息的函数
